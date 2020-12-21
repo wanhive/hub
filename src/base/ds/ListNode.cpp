ec8fe05b760e85221411b897961c8f4cf09190ea
Amit Kumar <amitkriit@gmail.com>
2020-12-21 23:35:56 +0530

    Update ListNode

--- src/base/ds/ListNode.cpp
+++ src/base/ds/ListNode.cpp
@@ -23,7 +23,7 @@ ListNode::~ListNode() {
 }
 
 bool ListNode::link(ListNode *head) noexcept {
-	if (!inList() && head) {
+	if (!isListed() && head) {
 		// --(head)-[this]-[n1]-[n2]--
 		setPredecessor(head);
 		setSuccessor(head->getSuccessor());
@@ -36,18 +36,15 @@ bool ListNode::link(ListNode *head) noexcept {
 	}
 }
 
-ListNode* ListNode::delink() noexcept {
-	if (inList()) {
+void ListNode::delink() noexcept {
+	if (isListed()) {
 		getSuccessor()->setPredecessor(getPredecessor());
 		getPredecessor()->setSuccessor(getSuccessor());
 		reset();
-		return this;
-	} else {
-		return nullptr;
 	}
 }
 
-bool ListNode::inList() const noexcept {
+bool ListNode::isListed() const noexcept {
 	return listed;
 }
 
@@ -55,19 +52,23 @@ ListNode* ListNode::getPredecessor() const noexcept {
 	return contiguous[0];
 }
 
+ListNode* ListNode::getSuccessor() const noexcept {
+	return contiguous[1];
+}
+
 void ListNode::setPredecessor(ListNode *node) noexcept {
 	if (node) {
 		contiguous[0] = node;
+	} else {
+		contiguous[0] = this;
 	}
-}
-
-ListNode* ListNode::getSuccessor() const noexcept {
-	return contiguous[1];
 }
 
 void ListNode::setSuccessor(ListNode *node) noexcept {
 	if (node) {
 		contiguous[1] = node;
+	} else {
+		contiguous[1] = this;
 	}
 }
 
--- src/base/ds/ListNode.h
+++ src/base/ds/ListNode.h
@@ -21,31 +21,25 @@ class ListNode {
 public:
 	ListNode() noexcept;
 	virtual ~ListNode();
-	//-----------------------------------------------------------------
 	/*
 	 * Inserts the node into a linked list after the <head>. Returns true on
 	 * success, false otherwise. Doesn't modify the ListNode::listed field of
 	 * the <head> (allows the <head> to mark the list boundaries).
 	 */
 	bool link(ListNode *head) noexcept;
-	/*
-	 * Removes the node from a linked list. Returns the pointer to this node
-	 * if it was in a list, nullptr otherwise.
-	 */
-	ListNode* delink() noexcept;
-	//Returns true if this node is in a linked list.
-	bool inList() const noexcept;
-	//-----------------------------------------------------------------
+	//Removes the node from it's linked list
+	void delink() noexcept;
+	//Returns true if this node is already in a linked list.
+	bool isListed() const noexcept;
 	//Returns the predecessor (nullptr if no predecessor)
 	ListNode* getPredecessor() const noexcept;
-	//Sets <node> as the predecessor (silently fails if <node> is nullptr)
-	void setPredecessor(ListNode *node) noexcept;
 	//Returns the successor (nullptr if no successor)
 	ListNode* getSuccessor() const noexcept;
-	//Sets <node> as the successor (silently fails if <node> is nullptr)
-	void setSuccessor(ListNode *node) noexcept;
-	//-----------------------------------------------------------------
 protected:
+	//Sets <node> as the predecessor (treats nullptr as this pointer)
+	void setPredecessor(ListNode *node) noexcept;
+	//Sets <node> as the successor (treats nullptr as this pointer)
+	void setSuccessor(ListNode *node) noexcept;
 	//Resets the object to it's initial state
 	void reset() noexcept;
 private:
