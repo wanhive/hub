8ddf04bad9767a0944a7872346ea01c46d801618
Amit <amitkriit@gmail.com>
2022-05-26 12:27:51 +0530

    Export Protocol.h
    Include Protocol.h in the top-level header file.

--- src/wanhive-hub.h
+++ src/wanhive-hub.h
@@ -13,6 +13,7 @@
 #ifndef WH_WANHIVE_HUB_H_
 #define WH_WANHIVE_HUB_H_
 
-#include "hub/ClientHub.h"   //Already includes Hub.h
+#include "hub/ClientHub.h"
+#include "hub/Protocol.h"
 
 #endif /* WH_WANHIVE_HUB_H_ */
