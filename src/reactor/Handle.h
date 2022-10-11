b58e73fba79c6d911818e65101300bf7a46dde3f
Amit <amitkriit@gmail.com>
2022-10-11 14:17:19 +0530

    Add generic Handle
    Generic Handle for selectvely passing on a value to a given module.

--- src/reactor/Handle.h
+++ src/reactor/Handle.h
@@ -12,13 +12,47 @@
 
 #ifndef WH_REACTOR_HANDLE_H_
 #define WH_REACTOR_HANDLE_H_
+#include "../base/common/pod.h"
 
 namespace wanhive {
 /**
- * File handle that selectively exposes file descriptor to a module
- * @tparam T friend class' name
+ * Generic handle that selectively exposes a value to a module.
+ * @tparam F friend class' name
+ * @tparam V data type
  */
-template<typename T> class Handle {
+template<typename F, typename V = int> class Handle {
+public:
+	/**
+	 * Constructor: assigns a value to this object.
+	 * @param v the value
+	 */
+	Handle(const V &data) noexcept :
+			data { data } {
+
+	}
+	/**
+	 * Destructor
+	 */
+	~Handle() = default;
+private:
+	/**
+	 * Returns the associated value.
+	 * @return the value
+	 */
+	const V& value() const noexcept {
+		return data;
+	}
+private:
+	WH_POD_ASSERT(V);
+	V data;
+	friend F;
+};
+
+/**
+ * File handle that selectively exposes file descriptor to a module.
+ * @tparam F friend class' name
+ */
+template<typename F> class Handle<F, int> {
 public:
 	/**
 	 * Constructor: assigns a file descriptor to this object.
@@ -41,7 +75,7 @@ private:
 	}
 private:
 	int fd;
-	friend T;
+	friend F;
 };
 
 } /* namespace wanhive */
