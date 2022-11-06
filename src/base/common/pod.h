a91c3bf559cb7506fefacdef24665ac388c0e8e9
Amit <amitkriit@gmail.com>
2022-11-07 00:03:21 +0530

    Update POD tests
    Use C++ 17 features to simplify the expressions.

--- src/base/common/pod.h
+++ src/base/common/pod.h
@@ -15,11 +15,11 @@
 #include "defines.h"
 #include <type_traits>
 
-#define WH_IS_POD(X) (std::is_trivial<X>::value && std::is_standard_layout<X>::value)
+#define WH_IS_POD(X) (std::is_trivial_v<X> && std::is_standard_layout_v<X>)
 #define WH_POD_ASSERT(X) WH_STATIC_ASSERT(WH_IS_POD(X), #X" should be POD (plain old data) type.")
 
-#define WH_IS_INTEGRAL(X) (std::is_integral<X>::value)
-#define WH_IS_POINTER(X) (std::is_pointer<X>::value)
+#define WH_IS_INTEGRAL(X) (std::is_integral_v<X>)
+#define WH_IS_POINTER(X) (std::is_pointer_v<X>)
 #define WH_IS_SCALAR(X) (WH_IS_INTEGRAL(X) || WH_IS_POINTER(X))
 
 #define WH_INTEGRAL_ASSERT(X) WH_STATIC_ASSERT(WH_IS_INTEGRAL(X), #X" should be integral type.")
