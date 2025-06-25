02e4a52fe104a8d5fa2bf991aa6b5784a36d72b4
Amit Kumar <amitkriit@gmail.com>
2025-06-26 03:23:55 +0530

    Pixel replication
    Add pixel replication routine to the Heatmap generator class.

--- src/base/ds/Gradient.cpp
+++ src/base/ds/Gradient.cpp
@@ -79,4 +79,21 @@ void Gradient::map(double data, Color &color) const noexcept {
 	}
 }
 
+void Gradient::replicate(unsigned char *out, Color color,
+		Planar<unsigned> pixel, Planar<unsigned> limits,
+		unsigned scale) noexcept {
+	size_t newWidth = limits.x * scale;
+	for (unsigned repY = 0; repY < scale; ++repY) {
+		for (unsigned repX = 0; repX < scale; ++repX) {
+			size_t newX = pixel.x * scale + repX;
+			size_t newY = pixel.y * scale + repY;
+
+			size_t idx = (newY * newWidth + newX) * 3;
+			out[idx] = color.red;
+			out[idx + 1] = color.green;
+			out[idx + 2] = color.blue;
+		}
+	}
+}
+
 } /* namespace wanhive */
