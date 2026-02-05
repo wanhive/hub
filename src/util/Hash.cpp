dce99a1ff5b8a03c9efd2f910e6770b4592d817a
Amit Kumar <amitkriit@gmail.com>
2026-02-05 13:21:03 +0530

    Update cryptographic hashing facility
    Expand the cryptographic hash interface to improve usability and type-safety.

--- src/util/Hash.cpp
+++ src/util/Hash.cpp
@@ -23,17 +23,62 @@ Hash::~Hash() {
 
 }
 
+bool Hash::create(const void *data, size_t bytes, Digest &digest) noexcept {
+	return sha.create(data, bytes, Hash::bytes(digest));
+}
+
 bool Hash::create(const void *data, size_t bytes, Digest *digest) noexcept {
-	return sha.create(data, bytes, *digest);
+	return sha.create(data, bytes, Hash::bytes(digest));
+}
+
+bool Hash::verify(const Digest &digest, const void *data, size_t bytes) noexcept {
+	return sha.verify(data, bytes, Hash::bytes(digest));
 }
 
 bool Hash::verify(const Digest *digest, const void *data, size_t bytes) noexcept {
-	return sha.verify(data, bytes, *digest);
+	return sha.verify(data, bytes, Hash::bytes(digest));
+}
+
+unsigned int Hash::encode(const Digest &digest, EncodedDigest &encoded) noexcept {
+	return Encoding::encode(ENC_BASE64, Hash::bytes(encoded),
+			Hash::bytes(digest), Hash::SIZE, sizeof(EncodedDigest));
 }
 
 unsigned int Hash::encode(const Digest *digest, EncodedDigest *encoded) noexcept {
-	return Encoding::encode(ENC_BASE64, *encoded, *digest, Hash::SIZE,
-			sizeof(EncodedDigest));
+	return Encoding::encode(ENC_BASE64, Hash::bytes(encoded),
+			Hash::bytes(digest), Hash::SIZE, sizeof(EncodedDigest));
+}
+
+unsigned char* Hash::bytes(Digest &digest) noexcept {
+	return digest;
+}
+
+unsigned char* Hash::bytes(Digest *digest) noexcept {
+	return digest ? (*digest) : nullptr;
+}
+
+const unsigned char* Hash::bytes(const Digest &digest) noexcept {
+	return digest;
+}
+
+const unsigned char* Hash::bytes(const Digest *digest) noexcept {
+	return digest ? (*digest) : nullptr;
+}
+
+char* Hash::bytes(EncodedDigest &encoded) noexcept {
+	return encoded;
+}
+
+char* Hash::bytes(EncodedDigest *encoded) noexcept {
+	return encoded ? (*encoded) : nullptr;
+}
+
+const char* Hash::bytes(const EncodedDigest &encoded) noexcept {
+	return encoded;
+}
+
+const char* Hash::bytes(const EncodedDigest *encoded) noexcept {
+	return encoded ? (*encoded) : nullptr;
 }
 
 } /* namespace wanhive */
--- src/util/Hash.h
+++ src/util/Hash.h
@@ -19,7 +19,7 @@ namespace wanhive {
 //-----------------------------------------------------------------
 /*! SHA-512, 64-bytes (512-bits) digest */
 using Digest = unsigned char[64];
-/*! Base-64 encoding of a raw Digest */
+/*! Base-64 encoding of a raw Digest (89 bytes used) */
 using EncodedDigest = char[128];
 //-----------------------------------------------------------------
 /**
@@ -43,7 +43,23 @@ public:
 	 * @param digest stores the message digest
 	 * @return true on success, false otherwise
 	 */
+	bool create(const void *data, size_t bytes, Digest &digest) noexcept;
+	/**
+	 * Generates input data's message digest.
+	 * @param data input data
+	 * @param bytes data size in bytes
+	 * @param digest stores the message digest
+	 * @return true on success, false otherwise
+	 */
 	bool create(const void *data, size_t bytes, Digest *digest) noexcept;
+	/**
+	 * Verifies input data's message digest.
+	 * @param digest message digest
+	 * @param data input data
+	 * @param bytes data size in bytes
+	 * @return true on successful verification, false otherwise
+	 */
+	bool verify(const Digest &digest, const void *data, size_t bytes) noexcept;
 	/**
 	 * Verifies input data's message digest.
 	 * @param digest message digest
@@ -59,11 +75,70 @@ public:
 	 * @param encoded stores the output
 	 * @return output size in bytes (excluding the nul-terminator)
 	 */
+	static unsigned int encode(const Digest &digest,
+			EncodedDigest &encoded) noexcept;
+	/**
+	 * Base-64 encodes a message digest.
+	 * @param digest message digest
+	 * @param encoded stores the output
+	 * @return output size in bytes (excluding the nul-terminator)
+	 */
 	static unsigned int encode(const Digest *digest,
 			EncodedDigest *encoded) noexcept;
+	//-----------------------------------------------------------------
+	/**
+	 * Type casts Digest to raw bytes.
+	 * @param digest message digest
+	 * @return pointer to raw bytes
+	 */
+	static unsigned char* bytes(Digest &digest) noexcept;
+	/**
+	 * Type casts Digest to raw bytes.
+	 * @param digest message digest
+	 * @return pointer to raw bytes
+	 */
+	static unsigned char* bytes(Digest *digest) noexcept;
+	/**
+	 * Type casts Digest to raw bytes.
+	 * @param digest message digest
+	 * @return pointer to raw bytes
+	 */
+	static const unsigned char* bytes(const Digest &digest) noexcept;
+	/**
+	 * Type casts Digest to raw bytes.
+	 * @param digest message digest
+	 * @return pointer to raw bytes
+	 */
+	static const unsigned char* bytes(const Digest *digest) noexcept;
+	/**
+	 * Type casts EncodedDigest to raw bytes.
+	 * @param digest base-64 encoded message digest
+	 * @return pointer to raw bytes
+	 */
+	static char* bytes(EncodedDigest &encoded) noexcept;
+	/**
+	 * Type casts EncodedDigest to raw bytes.
+	 * @param digest base-64 encoded message digest
+	 * @return pointer to raw bytes
+	 */
+	static char* bytes(EncodedDigest *encoded) noexcept;
+	/**
+	 * Type casts EncodedDigest to raw bytes.
+	 * @param digest base-64 encoded message digest
+	 * @return pointer to raw bytes
+	 */
+	static const char* bytes(const EncodedDigest &encoded) noexcept;
+	/**
+	 * Type casts EncodedDigest to raw bytes.
+	 * @param digest base-64 encoded message digest
+	 * @return pointer to raw bytes
+	 */
+	static const char* bytes(const EncodedDigest *encoded) noexcept;
 public:
 	/*! Message digest size in bytes **/
 	static constexpr unsigned int SIZE = Sha::length(WH_SHA512);
+	/*! Message digest's base-64 encoding size in bytes **/
+	static constexpr unsigned int ENCODING_SIZE = 89;
 private:
 	Sha sha;
 };
